62e8b9f47398e455306586c47f7bfb8f36cda165
Ennaram balamani <115247225+Ennarambalamani@users.noreply.github.com>
2022-10-18 19:05:52 +0530

    Add files via upload

---
+++ Distance.java
@@ -0,0 +1,12 @@
+class Distance{
+  public static void main(String[] args){
+    int a,b,c,d;
+    double dis;
+    a=3;
+    b=4;
+    c=5;
+    d=6;
+dis=Math.sqrt((b-a)*(b-a)+(d-c)*(d-c));
+System.out.println("distancebetween"+"("+a+","+c+"),"+"("+b+","+d+")"+dis);
+  }
+  }
